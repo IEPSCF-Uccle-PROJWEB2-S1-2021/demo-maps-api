66a3760ecbed8bf9227fd58b35ccb64e1e7a331a
Francois Roland <froland@froland.be>
2021-01-16 17:45:13 +0100

    Show a map centered on a latitude and a longitude.

---
+++ public/js/map-hello-world.js
@@ -0,0 +1,9 @@
+let map;
+
+function initMap() {
+  const mapDiv = document.getElementById('map');
+  map = new google.maps.Map(mapDiv, {
+    center: { lat: 50.80358094898162, lng: 4.32758324093389 },
+    zoom: 17,
+  });
+}
