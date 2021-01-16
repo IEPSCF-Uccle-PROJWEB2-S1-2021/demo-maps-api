3ed0e0550861e54c7d6d3ddd75c11bb19574e44e
Francois Roland <froland@froland.be>
2021-01-16 18:13:53 +0100

    Map current location.

---
+++ public/js/map-my-location.js
@@ -0,0 +1,44 @@
+let map;
+let infoWindow;
+
+function initMap() {
+  const mapDiv = document.getElementById('map');
+  map = new google.maps.Map(mapDiv, {
+    center: { lat: 50.80358094898162, lng: 4.32758324093389 },
+    zoom: 14,
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        const marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          title: 'Ma position',
+        });
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
